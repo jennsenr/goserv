36091ab442ae62569496f4e735eaf49b62e28253
Jennsen <jennsen.ramos@factum.es>
2024-10-20 11:53:04 +0200

    fix slash get full path

--- utils.go
+++ utils.go
@@ -8,18 +8,25 @@ import (
 
 func (s *Server) getFullPath(httpMethod, path string) string {
 	var fullPath string
+	
 	if s.basePath != "" && s.basePath != "/" {
 		fullPath = s.basePath + path
 	} else {
 		fullPath = path
 	}
 
-	return fmt.Sprintf("%s %s", httpMethod, strings.TrimSuffix(fullPath, "/"))
+	if len(fullPath) > 1 {
+		fullPath = strings.TrimSuffix(fullPath, "/")
+	}
+
+	return fmt.Sprintf("%s %s", httpMethod, fullPath)
 }
 
 func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		if len(r.URL.Path) > 1 {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
 
 		next.ServeHTTP(w, r)
 	})
